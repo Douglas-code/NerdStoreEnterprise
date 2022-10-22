81cf231764f3f1d760d7f6ef3b5c509e769807db
Douglas Rodrigues <douglasrodrigues20161@outlook.com>
2022-10-22 13:39:39 -0300

    adicionando autenticacao e aplicando boas praticas de configuracao do startup

---
+++ src/web/NSE.WebApp.MVC/Configuration/WebAppConfig.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace NSE.WebApp.MVC.Configuration
+{
+    public static class WebAppConfig
+    {
+        public static void AddMvcConfiguration(this IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public static void UseMvcConfiguration(this IApplicationBuilder applicationBuilder, IWebHostEnvironment environment)
+        {
+            if (environment.IsDevelopment())
+            {
+                applicationBuilder.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                applicationBuilder.UseExceptionHandler("/Home/Error");
+                applicationBuilder.UseHsts();
+            }
+            applicationBuilder.UseHttpsRedirection();
+            applicationBuilder.UseStaticFiles();
+
+            applicationBuilder.UseRouting();
+
+            applicationBuilder.UseIdentityConfiguration();
+
+            applicationBuilder.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
