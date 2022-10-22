6605f80788b8468d55e500c4f2c8a32d9ebbda78
Douglas Rodrigues <douglas.pereira@basis.com.br>
2022-10-22 12:34:16 -0300

    criação dos projetos e finalização da api de autenticação

---
+++ src/services/NSE.Identidade.API/Configuration/ApiConfig.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace NSE.Identidade.API.Configuration
+{
+    public static class ApiConfig
+    {
+        public static IServiceCollection AddApiConfiguration(this IServiceCollection services)
+        {
+            services.AddControllers();
+            return services;
+        }
+
+        public static IApplicationBuilder UseApiConfiguration(this IApplicationBuilder applicationBuilder, IWebHostEnvironment environment)
+        {
+            if (environment.IsDevelopment())
+            {
+                applicationBuilder.UseDeveloperExceptionPage();
+            }
+
+            applicationBuilder.UseHttpsRedirection();
+
+            applicationBuilder.UseRouting();
+
+            applicationBuilder.UseIdentityConfiguration();
+
+            applicationBuilder.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            return applicationBuilder;
+        }
+    }
+}
