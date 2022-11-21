e5c4bd6f52f944aadc5c3e4d2d941f9c02df8696
Douglas Rodrigues <douglasrodrigues20161@outlook.com>
2022-11-20 23:39:48 -0300

    refatorando servico http de autenticacao

--- src/web/NSE.WebApp.MVC/Services/Service.cs
+++ src/web/NSE.WebApp.MVC/Services/Service.cs
@@ -1,11 +1,23 @@
 ﻿using NSE.WebApp.MVC.Extensions;
-using System;
 using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace NSE.WebApp.MVC.Services
 {
     public abstract class Service
     {
+        protected StringContent ObterConteudo(object dado)
+        {
+            return new StringContent(JsonSerializer.Serialize(dado), Encoding.UTF8, "application/json");
+        }
+
+        protected async Task<T> DesearilizarObjetoResponse<T>(HttpResponseMessage responseMessage)
+        {
+            return JsonSerializer.Deserialize<T>(await responseMessage.Content.ReadAsStringAsync(), new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+
         protected bool TratarErrosResponse(HttpResponseMessage response)
         {
             switch ((int)response.StatusCode)
