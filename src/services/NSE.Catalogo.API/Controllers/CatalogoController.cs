0e5fb2b08bee638e970792560255aa595d53e109
Douglas Rodrigues <douglasrodrigues20161@outlook.com>
2022-11-29 23:23:01 -0300

    criado migrations e repositories

---
+++ src/services/NSE.Catalogo.API/Controllers/CatalogoController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSE.Catalogo.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NSE.Catalogo.API.Controllers
+{
+    [ApiController]
+    public class CatalogoController : ControllerBase
+    {
+        private readonly IProdutoRepository _produtoRepository;
+
+        public CatalogoController(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        [HttpGet("catalogo/produtos")]
+        public async Task<IEnumerable<Produto>> Produtos()
+        {
+            return await _produtoRepository.ObterTodos();
+        }
+
+        [HttpGet("catalogo/produtos/{id}")]
+        public async Task<Produto> ProdutoDetalhe(Guid id)
+        {
+            return await _produtoRepository.ObterPorId(id);
+        }
+
+        [HttpPost("catalogo/produto")]
+        public async Task<IActionResult> ProdutoDetalhe(Produto produto)
+        {
+            _produtoRepository.Adicionar(produto);
+
+            return Ok();
+        }
+    }
+}
