3dee5640200fe4299c4e246822e69e69a38cf214
Douglas Rodrigues <douglasrodrigues20161@outlook.com>
2023-02-07 21:16:32 -0300

    criar entidades e objetos de valor da api de clientes

---
+++ src/building blocks/NSE.Core/DomainObjects/Email.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace NSE.Core.DomainObjects
+{
+    public class Email
+    {
+        public Email() { }
+
+        public Email(string endereco)
+        {
+            if (!Validar(endereco)) throw new DomainException("Email inválido");
+            Endereco = endereco;
+        }
+
+        public const int EnderecoMaxLength = 254;
+        public const int EnderecoMinLength = 5;
+        public string Endereco { get; private set; }
+
+        public static bool Validar(string email)
+        {
+            var regexEmail = new Regex(@"^ ([\w\.\-] +)@([\w\-] +)((\.(\w){ 2, 3 })+)$");
+            return regexEmail.IsMatch(email);
+        }
+    }
+}
