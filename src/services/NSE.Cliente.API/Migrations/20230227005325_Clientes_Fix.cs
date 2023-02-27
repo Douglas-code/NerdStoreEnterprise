d3439cc01d06301693fce3c14b10a661f036b4fd
Douglas Rodrigues <douglasrodrigues20161@outlook.com>
2023-02-26 22:01:32 -0300

    Mapear entidades da Api de clientes e configurar os servicoes

---
+++ src/services/NSE.Cliente.API/Migrations/20230227005325_Clientes_Fix.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NSE.Clientes.API.Migrations
+{
+    public partial class Clientes_Fix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "varchar(200)",
+                table: "Clientes",
+                newName: "Nome");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Clientes",
+                type: "varchar(200)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Nome",
+                table: "Clientes",
+                newName: "varchar(200)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "varchar(200)",
+                table: "Clientes",
+                type: "varchar(100)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(200)");
+        }
+    }
+}
