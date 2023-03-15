b37a111a3357d0af343edbc502e146bde74df39b
Douglas Rodrigues <douglasrodrigues20161@outlook.com>
2023-03-14 22:01:57 -0300

    adicionar estrtura padrão de commands, events, command handler e cricao do primeiro command handler

---
+++ src/services/NSE.Cliente.API/Application/Commands/ClienteCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation.Results;
+using MediatR;
+using NSE.Clientes.API.Models;
+using NSE.Core.Messages;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NSE.Clientes.API.Application.Commands
+{
+    public class ClienteCommandHandler : CommandHandler, IRequestHandler<RegistrarClienteCommand, ValidationResult>
+    {
+        public async Task<ValidationResult> Handle(RegistrarClienteCommand request, CancellationToken cancellationToken)
+        {
+            if (request.EhValido()) return request.ValidationResult;
+
+            var cliente = new Cliente(request.Id, request.Nome, request.Email, request.Cpf);
+
+            if (true) // Já existe cliente com o cpf informado
+            {
+                AdicionarErro("Cliente já existe!");
+                return ValidationResult;
+            }
+
+            return request.ValidationResult;
+        }
+    }
+}
--- src/services/NSE.Cliente.API/Configuration/DependencyInjectionConfig.cs
+++ src/services/NSE.Cliente.API/Configuration/DependencyInjectionConfig.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation.Results;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using NSE.Clientes.API.Application.Commands;
+using NSE.Core.Mediator;
 
 namespace NSE.Clientes.API.Configuration
 {
@@ -6,7 +10,8 @@ namespace NSE.Clientes.API.Configuration
     {
         public static void RegisterServices(this IServiceCollection services)
         {
-
+            services.AddScoped<IMediatorHandler, MediatorHandler>();
+            services.AddScoped<IRequestHandler<RegistrarClienteCommand, ValidationResult>, ClienteCommandHandler>();
         }
     }
 }
