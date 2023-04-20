84a89df1e4d182e2dfa8d38e8cb9e5aa5e33c9be
Douglas Rodrigues <douglasrodrigues20161@outlook.com>
2023-04-19 21:10:48 -0300

    refatorar MainController pra todas as Controlllers de todas as apis conseguirem usar a mesma implementacao

---
+++ src/services/NSE.Cliente.API/Controllers/ClientesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using NSE.Identidade.API.Controllers;
+
+namespace NSE.Clientes.API.Controllers
+{
+    [Authorize]
+    public class ClientesController : MainController
+    {
+    }
+}
