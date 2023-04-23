e3f86acc1d86b8391f6baea1d96aa458ab3f0d7d
Douglas Rodrigues <douglasrodrigues20161@outlook.com>
2023-04-23 11:26:10 -0300

    remover suporte ao docker

--- src/services/NSE.Cliente.API/Controllers/ClientesController.cs
+++ src/services/NSE.Cliente.API/Controllers/ClientesController.cs
@@ -1,10 +1,25 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NSE.Clientes.API.Application.Commands;
+using NSE.Core.Mediator;
 using NSE.Identidade.API.Controllers;
+using System.Threading.Tasks;
 
 namespace NSE.Clientes.API.Controllers
 {
-    [Authorize]
     public class ClientesController : MainController
     {
+        private readonly IMediatorHandler _mediatorHandler;
+
+        public ClientesController(IMediatorHandler mediatorHandler)
+        {
+            _mediatorHandler = mediatorHandler;
+        }
+
+        [HttpGet("clientes")]
+        public async Task<IActionResult> Index()
+        {
+            return CustomResponse(await _mediatorHandler.EnviarComando(new RegistrarClienteCommand(System.Guid.NewGuid(), "Testenjasvjdasjdasdlasda", "meuemail@gmail.com", "68070291010")));
+        }
     }
 }
