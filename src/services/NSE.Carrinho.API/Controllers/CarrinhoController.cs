0ec54ab82bb6acd37a15b5bf74a179241f568a22
Douglas Rodrigues <douglasrodrigues20161@outlook.com>
2023-05-24 21:04:19 -0300

    configurar estrutura padrao api de carrinho

---
+++ src/services/NSE.Carrinho.API/Controllers/CarrinhoController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using NSE.Identidade.API.Controllers;
+
+namespace NSE.Carrinho.API.Controllers
+{
+    [Authorize]
+    public class CarrinhoController : MainController
+    {
+    }
+}
